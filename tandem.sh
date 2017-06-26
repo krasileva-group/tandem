e820c5cfce7ccc2547b8b77f134588b6c4cb26fa
Christian Schudoma <cschu1981@gmail.com>
2017-06-26 23:35:55 +0100

    Fixed issue with gff files that a) contain features other than gene and/or b) are unsorted/have out of order records.

--- tandem.sh
+++ tandem.sh
@@ -61,7 +61,8 @@ fi
 # https://stackoverflow.com/questions/18709507/maximum-and-minimum-using-awk
 
 grep -F -f $1 $2 > $OUT_FILE_PREFIX.query.gff3
-bedtools closest -d -k 2 -a $OUT_FILE_PREFIX.query.gff3 -b $2 | awk -v OFS='\t' '{ if ($19 == 0) { if ($13 >= $4) { $19 = -((($5<$14)?$5:$14) - $13 + 1); } else { $19 = -((($5<$14)?5:$14) - $4 + 1); } }; print $0; }' | awk -v OFS='\t' -v min_ovl=$MIN_OVERLAP -v max_dst=$MAX_DISTANCE '{ if (((min_ovl <= -($19))||(min_ovl == 0 && $19 <= max_dst)) && ($4 != $13 || $5 != $14)) { split($9, info1, ";"); split($18, info2, ";"); print $1,$4,$5,$7,$13,$14,$16,substr(info1[1], 4),substr(info2[1], 4),$19; } }' > $OUT_FILE_PREFIX.tmp.tsv
+#exit
+bedtools closest -d -k 2 -a <(grep $'\t'gene$'\t' $OUT_FILE_PREFIX.query.gff3 | sort -k1,1 -k4,4n) -b <(grep $'\t'gene$'\t' $2 | sort -k1,1 -k4,4n) | awk -v OFS='\t' '{ if ($19 == 0) { if ($13 >= $4) { $19 = -((($5<$14)?$5:$14) - $13 + 1); } else { $19 = -((($5<$14)?5:$14) - $4 + 1); } }; print $0; }' | awk -v OFS='\t' -v min_ovl=$MIN_OVERLAP -v max_dst=$MAX_DISTANCE '{ if (((min_ovl <= -($19))||(min_ovl == 0 && $19 <= max_dst)) && ($4 != $13 || $5 != $14)) { split($9, info1, ";"); split($18, info2, ";"); print $1,$4,$5,$7,$13,$14,$16,substr(info1[1], 4),substr(info2[1], 4),$19; } }' > $OUT_FILE_PREFIX.tmp.tsv
 
 
 #Triticum_aestivum_CS42_TGACv1_scaffold_061102_1DL	82104	90815	-	80705	81710	+	TRIAE_CS42_1DL_TGACv1_061102_AA0185320	TRIAE_CS42_1DL_TGACv1_061102_AA0185310	394
